93e5cd72a2672479934e4af638f3b4b94416e533
Alex van Niekerk <vanniekerk.alex@gmail.com>
2022-02-09 15:35:22 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import StaticDateRangePicker from '@mui/lab/StaticDateRangePicker';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function App() {
+  const [value, setValue] = React.useState([null, null]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <div class="container">
+          <h1>Leave Days</h1>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <StaticDateRangePicker
+              displayStaticWrapperAs="desktop"
+              calendars="1"
+              disableMonthSwitching="true"
+              value={value}
+              onChange={(newValue) => {
+                setValue(newValue);
+              }}
+              renderInput={(startProps, endProps) => (
+                <React.Fragment>
+                  <TextField {...startProps} />
+                  <Box sx={{ mx: 2 }}> to </Box>
+                  <TextField {...endProps} />
+                </React.Fragment>
+              )}
+            />
+          </LocalizationProvider>
+        </div>
+      </Container>
+    </>
   );
 }
 
