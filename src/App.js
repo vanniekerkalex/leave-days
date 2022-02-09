e46ba3d3ae6afd0e907919347464795c371b6e84
Alex van Niekerk <vanniekerk.alex@gmail.com>
2022-02-09 15:55:46 +0100

    Testing date range picker.

--- src/App.js
+++ src/App.js
@@ -9,31 +9,64 @@ import Container from '@mui/material/Container';
 
 function App() {
   const [value, setValue] = React.useState([null, null]);
+
+  const year = new Date().getFullYear();
+
   return (
     <>
       <Container>
-        <div class="container">
-          <h1>Leave Days</h1>
-          <p>To help you get the most out of your leave days.</p>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <StaticDateRangePicker
-              displayStaticWrapperAs="desktop"
-              calendars="1"
-              disableMonthSwitching="true"
-              value={value}
-              onChange={(newValue) => {
-                setValue(newValue);
-              }}
-              renderInput={(startProps, endProps) => (
-                <React.Fragment>
-                  <TextField {...startProps} />
-                  <Box sx={{ mx: 2 }}> to </Box>
-                  <TextField {...endProps} />
-                </React.Fragment>
-              )}
-            />
-          </LocalizationProvider>
-        </div>
+        <h1>Leave Days</h1>
+        <p>To help you get the most out of your leave days.</p>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <StaticDateRangePicker
+            displayStaticWrapperAs="desktop"
+            calendars={1}
+            value={value}
+            defaultCalendarMonth={new Date(year, 0)}
+            onChange={(newValue) => {
+              setValue(newValue);
+            }}
+            renderInput={(startProps, endProps) => (
+              <React.Fragment>
+                <TextField {...startProps} />
+                <Box sx={{ mx: 2 }}> to </Box>
+                <TextField {...endProps} />
+              </React.Fragment>
+            )}
+          />
+          <StaticDateRangePicker
+            displayStaticWrapperAs="desktop"
+            calendars={1}
+            value={value}
+            defaultCalendarMonth={new Date(year, 1)}
+            onChange={(newValue) => {
+              setValue(newValue);
+            }}
+            renderInput={(startProps, endProps) => (
+              <React.Fragment>
+                <TextField {...startProps} />
+                <Box sx={{ mx: 2 }}> to </Box>
+                <TextField {...endProps} />
+              </React.Fragment>
+            )}
+          />
+          <StaticDateRangePicker
+            displayStaticWrapperAs="desktop"
+            calendars={1}
+            value={value}
+            defaultCalendarMonth={new Date(year, 2)}
+            onChange={(newValue) => {
+              setValue(newValue);
+            }}
+            renderInput={(startProps, endProps) => (
+              <React.Fragment>
+                <TextField {...startProps} />
+                <Box sx={{ mx: 2 }}> to </Box>
+                <TextField {...endProps} />
+              </React.Fragment>
+            )}
+          />
+        </LocalizationProvider>
       </Container>
     </>
   );
